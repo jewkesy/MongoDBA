6de9b0c02fcf194744ea10daeeba606d3774e237
Daryl Jewkes <github@daryljewkes.com>
2014-06-18 08:18:16 +0100

    answered question 6

---
+++ final_exam/question_6/result.js
@@ -0,0 +1,15 @@
+Final: Question 6
+
+Which of these statements is true?
+
+Note: to get a multiple answer question right in this final you must get all the components right, so even if some parts are simple, take your time.
+
+
+
+	MongoDB (v2.2) supports transactions spanning multiple documents, if those documents all reside on the same shard.
+
+X	MongoDB supports atomic operations on individual documents.
+
+	MongoDB allows you to change the storage engine on a collection level.
+
+X	MongoDB has a data type for dates and datetime data.
