df43f60d1b3447baeb7e207d2d213d3a492f90fe
Daryl Jewkes <github@daryljewkes.com>
2014-06-18 08:30:52 +0100

    answered question 7

---
+++ final_exam/question_7/result.js
@@ -0,0 +1,10 @@
+Final: Question 7
+
+Which of these statements is true?
+
+
+	MongoDB is "multi-master" -- you can write anywhere, anytime.
+
+X	MongoDB supports reads from slaves/secondaries that are in remote locations.
+
+	Most MongoDB queries involve javascript execution on the database server(s).
